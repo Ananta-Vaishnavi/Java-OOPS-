9189eaf263da6f5d91ef12086575a7a5b79396b4
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2023-02-06 11:47:12 +0530

    Create Sum Average.java

---
+++ Basic/Sum Average.java
@@ -0,0 +1,19 @@
+class SumAverage
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(Sytem.in);
+	    int n1,n2,n3,n4,n5;
+	    int sum,average;
+	    n1 = sc.nextInt();
+	    n2 = sc.nextInt();
+	    n3 = sc.nextInt();
+	    n4 = sc.nextInt();
+	    n5 =sc.nextInt();
+	    sum = n1+n2+n3+n4+n5;
+	    average = sum/5
+	    System.out.println("Sum"+sum);
+	    System.out.println("Average"+average);
+	}
+	
+}
