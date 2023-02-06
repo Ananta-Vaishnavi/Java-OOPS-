e056a84b3a94e9a6dd2960b88f056cdeaa9b50cc
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2023-02-06 09:37:25 +0530

    Update Greatest.java

--- Basic/Greatest/Greatest.java
+++ Basic/Greatest/Greatest.java
@@ -4,7 +4,7 @@ class Greatest{
     {
         Scanner sc = new Scanner(System.in);
         int n1,n2,n3;
-        System.out("Enter three numbers");
+        System.out.println("Enter three numbers");
         n1 = sc.nextInt();
         n2 = sc.nextInt();
         n3 = sc.nextInt();
