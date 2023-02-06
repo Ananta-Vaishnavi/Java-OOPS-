cb9856c5e8df865dfdefbebcf8838d7b76994281
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2023-02-06 12:49:15 +0530

    Update Sum Average.java

--- Basic/Sum Average.java
+++ Basic/Sum Average.java
@@ -1,19 +1,20 @@
-class SumAverage
+import java.util.Scanner;
+
+public class SumAverage
 {
-	public static void main(String[] args) 
-	{
-	    Scanner sc = new Scanner(Sytem.in);
-	    int n1,n2,n3,n4,n5;
-	    int sum,average;
-	    n1 = sc.nextInt();
-	    n2 = sc.nextInt();
-	    n3 = sc.nextInt();
-	    n4 = sc.nextInt();
-	    n5 =sc.nextInt();
-	    sum = n1+n2+n3+n4+n5;
-	    average = sum/5
-	    System.out.println("Sum"+sum);
-	    System.out.println("Average"+average);
-	}
-	
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, n3, n4, n5;
+        int sum, average;
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+        n4 = sc.nextInt();
+        n5 = sc.nextInt();
+        sum = n1 + n2 + n3 + n4 + n5;
+        average = sum / 5;
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
 }
