33a1bcfd3ebc0244bbfd11fe8614846ae4bee9e2
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2023-02-06 11:00:40 +0530

    Create Days.java

---
+++ Basic/Days.java
@@ -0,0 +1,36 @@
+import java.lang.Math;
+import java.util.*;
+
+class Days {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int day;
+        day = sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+             case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+             case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+             case 7:
+                System.out.println("Thrusday");
+                break;
+          
+        }
+    
+    }
+}
