be2e4579c90a63e40c1427fb9d1e69c32c41b0ac
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2023-02-27 12:34:56 +0530

    Update Library.java

--- OOPs/Library.java
+++ OOPs/Library.java
@@ -1,30 +1,35 @@
 // Program to implement a Book class that stores the details of a book such as its code, title
 // and price (Use constructors to initialize the objects).
 import java.util.*;
-class Book
-{
+
+class Book {
     String code;
     String title;
     double price;
 
-    void Book(String code, String title, double price)
-    {
+    public Book(String code, String title, double price) {
         this.code = code;
         this.title = title;
         this.price = price;
     }
 
+    public void display() {
+        System.out.println("Code: " + code);
+        System.out.println("Title: " + title);
+        System.out.println("Price: " + price);
+    }
 }
-class Library{
-    public static void main(String args[])
-    {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the code of the book");
-       String code = sc.nextLine();
-       System.out.println("Enter the title of the book");
-       String title = sc.nextLine(); 
-       System.out.println("Enter the price of the book");
-       double price = sc.nextDouble();
-       Book b = new Book(code, title, price);
+
+class Library {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the code of the book");
+        String code = sc.nextLine();
+        System.out.println("Enter the title of the book");
+        String title = sc.nextLine();
+        System.out.println("Enter the price of the book");
+        double price = sc.nextDouble();
+        Book b = new Book(code, title, price);
+        b.display();
     }
 }
